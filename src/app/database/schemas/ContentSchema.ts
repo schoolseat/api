ba9fd3a18bdedfcef4d2182d771c33a75f79509f
Chronous <lucasmts2017@outlook.com>
2021-08-16 11:11:23 -0300

    ♻️ - classe method reworked, avaliation added

--- src/app/database/schemas/ContentSchema.ts
+++ src/app/database/schemas/ContentSchema.ts
@@ -5,5 +5,10 @@ export const ContentSchema = new Schema<Content & Document>({
   title: { type: String, required: true },
   content: { type: String, required: true },
   text: { type: String, required: true },
-  classe: { type: String, required: true },
+  classe: {
+    type: String,
+    ref: 'Classe',
+    required: true,
+  },
+  avaliation: { type: Object, required: false },
 })
--- src/app/database/schemas/LessonSchema.ts
+++ src/app/database/schemas/LessonSchema.ts
@@ -2,9 +2,15 @@ import { Schema, Document } from '@/app/database/Schema'
 import type { Lesson } from '@/types/models'
 
 export const LessonSchema = new Schema<Lesson & Document>({
-  deadline: { type: String, required: true },
-  title: { type: String, required: true },
-  content: { type: String, required: true },
   text: { type: String, required: true },
+  title: { type: String, required: true },
+  classe: {
+    type: String,
+    ref: 'Classe',
+    required: true,
+  },
+  content: { type: String, required: true },
+  deadline: { type: String, required: true },
   activityLink: { type: String, required: true },
+  avaliation: { type: Object, required: false },
 })
