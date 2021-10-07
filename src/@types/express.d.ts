c3e4eb6cb81f23d4a9c256559ed3e94cc350f930
drethenrain <drethenrain@hotmail.com>
2021-10-06 23:46:39 -0300

    :label: Added types at 'request.user'

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,5 +1,17 @@
 declare namespace Express {
   interface Request {
-    user
+    user: {
+      _id: string
+      password: string
+      email: string
+      name: string
+      nickname?: string
+      profilePic?: string
+      bornDate: string
+      bio: string
+      stars: string
+      level: string
+      xp: string
+    }
   }
 }
